27fae51cfd9110eb7fd9acdf2cf886c8b02a9b57
nythril <nythril@gnar>
2017-04-14 16:40:16 +0100

    Started launcher

---
+++ src/tech/tora/quaver/Launcher.java
@@ -0,0 +1,13 @@
+package tech.tora.quaver;
+
+public class Launcher {
+
+	public Launcher() {
+		System.out.println("Launching Quaver");
+	}
+
+	public static void main(String[] args) {
+		new Launcher();
+	}
+	
+}
