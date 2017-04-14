3f27bfbea203f664e929d01d460954041cea80d7
nythril <nythril@gnar>
2017-04-14 17:09:51 +0100

    Created template interface with standardised insets border. Launcher now shows interface without checks

--- src/tech/tora/quaver/Launcher.java
+++ src/tech/tora/quaver/Launcher.java
@@ -1,13 +1,66 @@
 package tech.tora.quaver;
 
+import java.io.File;
+
+import tech.tora.quaver.notepad.Interface;
+
 public class Launcher {
 
 	public Launcher() {
-		System.out.println("Launching Quaver");
+		
+		System.out.println("Launching");
+		
+		if (fileExists("res/config.json")) {
+			System.out.println("Config File Found");
+			
+			
+			
+		} else {
+			System.out.println("Config File Not Found");
+			
+			new Interface();
+			
+//			int failedConfigDialog = JOptionPane.showConfirmDialog(null, "The config file could not be found.\nA fresh config file can be created.\nCreate now?", "Config File Not Found", JOptionPane.OK_CANCEL_OPTION);
+//
+//			switch (failedConfigDialog) {
+//				case 0:
+//					// create config file
+//					System.out.println("Creating New Config File");
+//					
+//					break;
+//				case 2:
+//					Launcher.exit(0, "Closing as no config file found and no new one created");
+//					break;
+//				default:
+//					System.out.println("Trancendence");
+//					break;
+//			}
+			
+		}
+		
 	}
-
+	
+	/**
+	 * Simple check if a file exists
+	 * 
+	 * @param location - path to file
+	 * @return Returns true if the file exists
+	 */
+	public boolean fileExists(String location) {
+		File f = new File(location);
+		if(f.exists() && !f.isDirectory()) return true;
+		else return false;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		new Launcher();
+	}
+
+	public static void exit(int status, String reason) {
+		System.out.println("Exiting: " + reason);
+		System.exit(status);
 	}
 	
 }
