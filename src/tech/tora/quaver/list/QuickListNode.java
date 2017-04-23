c2b1fc0bef1067ac3afeb8c08250ce245ef5e89f
Nythril <Nythril@Bastion>
2017-04-23 16:34:05 +0100

    Added folder icon to each node under notebooks

--- src/tech/tora/quaver/list/QuickListNode.java
+++ src/tech/tora/quaver/list/QuickListNode.java
@@ -2,12 +2,19 @@ package tech.tora.quaver.list;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -24,7 +31,7 @@ public class QuickListNode extends JPanel {
 	private int width = 200;
 	private int height = 25;
 	
-	public QuickListNode(QuickList parent, String image, String title, String rightTitle) {
+	public QuickListNode(QuickList parent, String image, String title, String rightSide) {
 		this.parentList = parent;
 		setLayout(new BorderLayout());
 		setSize(width, height);
@@ -34,19 +41,33 @@ public class QuickListNode extends JPanel {
 		setBackground(new Color(230, 230, 230));
 		setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, new Color(200, 200, 200)));
 		
-		JLabel label1 = new JLabel(title);
+		JLabel label1 = new JLabel("  " + title);
 		label1.setFont(new Font("Helvetica", Font.BOLD, 12));
 		label1.setForeground(new Color(40, 40, 40));
+
+		JLabel label2 = new JLabel(rightSide);
+		label2.setFont(new Font("Helvetica", Font.BOLD, 12));
+		label2.setForeground(new Color(40, 40, 40));
 		
 		JPanel node1padded = new JPanel();
 		node1padded.setLayout(new BorderLayout());
-		node1padded.setBorder(BorderFactory.createEmptyBorder(2, 10, 0, 0));
+		node1padded.setBorder(BorderFactory.createEmptyBorder(2, 10, 0, 10));
 		node1padded.setSize(width, height);
 		node1padded.setPreferredSize(new Dimension(width, height));
 		node1padded.setMinimumSize(new Dimension(width, height));
 		node1padded.setMaximumSize(new Dimension(width, height));
 		node1padded.setBackground(new Color(230, 230, 230));
 		node1padded.add(label1, BorderLayout.CENTER);
+		node1padded.add(label2, BorderLayout.EAST);
+		
+		BufferedImage myPicture = null;
+		try {
+			myPicture = ImageIO.read(new File("res/icon1.png"));
+			JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+			node1padded.add(picLabel, BorderLayout.WEST);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		add(node1padded);
 		
@@ -84,7 +105,6 @@ public class QuickListNode extends JPanel {
 				
 			}
 		});
-		
 		
 	}
 	
