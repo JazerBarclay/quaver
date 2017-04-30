43e4a4a79b47e1f474e8424076192b5ba10b5326
Nythril <Nythril@Bastion>
2017-05-01 00:06:14 +0100

    Added handling of empty dialog boxes and configuration handling

--- src/tech/tora/quaver/Launcher.java
+++ src/tech/tora/quaver/Launcher.java
@@ -1,12 +1,17 @@
 package tech.tora.quaver;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 import javax.swing.JOptionPane;
+
+import org.json.simple.parser.ParseException;
 
 import tech.tora.quaver.config.Settings;
 import tech.tora.quaver.config.SettingsManager;
 import tech.tora.quaver.notepad.Interface;
+import tech.tora.quaver.structures.NotebookManager;
 
 public class Launcher {
 
@@ -20,7 +25,14 @@ public class Launcher {
 		if (fileExists("res/config.json")) {
 			System.out.println("Config File Found");
 			
-			// TODO read in config file
+			try {
+				globalSettings = SettingsManager.readConfigJSON();
+			} catch (IOException | ParseException e) {
+				e.printStackTrace();
+				Launcher.exit(1, "Failed to read config file");
+			}
+			
+			new Interface();
 			
 		} else {
 			System.out.println("Config File Not Found");
@@ -29,15 +41,12 @@ public class Launcher {
 
 			switch (failedConfigDialog) {
 				case 0:
-					// create config file
 					System.out.println("Creating New Config File");
-					
 					if (createConfigFile()) {
 						new Interface();
 					} else {
 						Launcher.exit(0, "Failed to create config file");
 					}
-					
 					break;
 				case 2:
 					Launcher.exit(0, "Closing as no config file found and no new one created");
@@ -65,7 +74,7 @@ public class Launcher {
 	}
 	
 	/**
-	 * Simple check if a file exists
+	 * Simple check if a directory exists
 	 * 
 	 * @param location - path to file
 	 * @return Returns true if the file exists
@@ -80,16 +89,9 @@ public class Launcher {
 		
 		Settings tmpSettings = new Settings();
 		
-		String configName = JOptionPane.showInputDialog(null, 
-				"Please enter the name of this configuration", "Configuration Settings", JOptionPane.NO_OPTION);
-		
-		String firstNotebookName = null;
-		while (firstNotebookName != null) {
-			firstNotebookName = JOptionPane.showInputDialog(null, 
-				"Please enter the name of your first notebook", "Create your first notebook", JOptionPane.NO_OPTION);
-		}
-		String firstNotebookLocation = JOptionPane.showInputDialog(null, 
-				"Please enter the location of your notebook", "Create your first notebook", JOptionPane.NO_OPTION);
+		String configName = generateConfigName();
+		String firstNotebookName = generateNotebookName();
+		String firstNotebookLocation = generateNotebookLocation();
 
 		// Drop the last character if its a / or a \\
 		if (firstNotebookLocation.substring(firstNotebookLocation.length()-1, firstNotebookLocation.length()).equals("/") || 
@@ -98,17 +100,74 @@ public class Launcher {
 		}
 		
 		tmpSettings.settings_title= configName;
-		
-//		if (dirExists()) {
-//			
-//		}
-		
 		tmpSettings.notebooks = new String[] {firstNotebookLocation + "/" + firstNotebookName + ".qvnotebook"};
 		
-		return false;
+		// write config file using what we have here
+		tmpSettings.devmode = false;
+		try {
+			SettingsManager.writeConfigJSON(tmpSettings);
+		} catch (IOException e) {
+			e.printStackTrace();
+			Launcher.exit(1, "Failed to write config file");
+		}
+		
+		// write first notebook folder and files
+		try {
+			NotebookManager.writeNotebookJSON(firstNotebookName, UUID.nameUUIDFromBytes(firstNotebookName.getBytes()).toString(), tmpSettings.notebooks[0]);
+		} catch (IOException e) {
+			e.printStackTrace();
+			Launcher.exit(1, "Failed to write first notebook");
+		}
+		
+		globalSettings = tmpSettings;
+		
+		return true;
 	}
 	
+	private String generateConfigName() {
+		String configName;
+		
+		configName = JOptionPane.showInputDialog(null, 
+				"Please enter the name of this configuration", "Configuration Settings", JOptionPane.NO_OPTION);
+		
+		if (configName == null) {
+			Launcher.exit(1, "Cancelled creation of new config");
+		} else if (configName.equals("")) { 
+			configName = generateConfigName();
+		}
+		
+		return configName;
+	}
 	
+	private String generateNotebookName() {
+		String notebookName;
+		
+		notebookName = JOptionPane.showInputDialog(null, 
+				"Please enter the name of your first notebook", "Create your first notebook", JOptionPane.NO_OPTION);
+		
+		if (notebookName == null) {
+			Launcher.exit(1, "Cancelled creation of new notebook");
+		} else if (notebookName.equals("")) {
+			notebookName = generateNotebookName();
+		}
+		
+		return notebookName;
+	}
+	
+	private String generateNotebookLocation() {
+		String notebookLocation;
+		
+		notebookLocation = JOptionPane.showInputDialog(null, 
+				"Please enter the location of your notebook", "Create your first notebook", JOptionPane.NO_OPTION);
+		
+		if (notebookLocation == null) {
+			Launcher.exit(1, "Cancelled creation of new notebook");
+		} else if (notebookLocation.equals("")) {
+			notebookLocation = generateNotebookLocation();
+		}
+		
+		return notebookLocation;
+	}
 	
 	public static void main(String[] args) {
 		new Launcher();
