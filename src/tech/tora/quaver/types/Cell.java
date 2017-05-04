59561248d1c36c774fec8f3b5fc173e762e8841a
Nythril <Nythril@Bastion.Home>
2017-05-04 03:31:04 +0100

    Added constructors

--- src/tech/tora/quaver/types/Cell.java
+++ src/tech/tora/quaver/types/Cell.java
@@ -6,4 +6,15 @@ public class Cell {
 	public String language;
 	public String data;
 	
+	public Cell() {
+		// Do Nothing
+	}
+	
+	public Cell(String type, String language, String data) {
+		this.type = type;
+		this.language = language;
+		this.data = data;
+	}
+	
+	
 }
