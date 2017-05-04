1038e6f07b1f1cc5fd8628362824591cee195e26
Nythril <Nythril@Bastion.Home>
2017-05-04 03:25:40 +0100

    New note class to replace the old mess

---
+++ src/tech/tora/quaver/types/Note.java
@@ -0,0 +1,99 @@
+package tech.tora.quaver.types;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import tech.tora.quaver.Launcher;
+import tech.tora.quaver.structures.CellMeta;
+
+public class Note {
+
+	public String path = "";
+	public String title = "";
+	public String uuid = "";
+	public long created_at = 0, updated_at = 0;
+	public CellMeta[] cells = new CellMeta[] {};
+	public String[] tags = new String[] {};
+
+	public Note() {
+		// Do nothing
+	}
+	
+	public Note(String path, String title, String uuid, long created, long updated, String...tags) {
+		this.path = path;
+		this.title = title;
+		this.uuid = uuid;
+		this.created_at = created;
+		this.updated_at = updated;
+		this.tags = tags;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void writeContentJSON(Note note) throws IOException {
+		JSONObject obj = new JSONObject();
+		obj.put("title", note.title);
+		System.out.println("Title Added: " + note.title);
+
+		JSONArray cells = new JSONArray();
+
+		System.out.println("Total Cells: " + note.cells.length);
+
+		for (CellMeta c2: note.cells) {
+			JSONObject c = new JSONObject();
+			c.put("type", c2.type);
+			System.out.println("Cell Type: " + c2.type);
+			c.put("language", c2.language);
+			System.out.println("Cell Lang: " + c2.language);
+			c.put("data", c2.data);
+			System.out.println("Cell Data: Just Trust");
+			cells.add(c);
+		}
+
+		System.out.println("Added");
+		obj.put("cells", cells);
+
+		if (!new File(note.path + Launcher.pathSeparator + note.uuid).exists()) {
+			if (!new File(note.path + Launcher.pathSeparator + note.uuid).mkdirs()) 
+				throw new IOException("Failed to create path " + note.path + Launcher.pathSeparator + note.uuid);
+		}
+		
+		try (FileWriter file = new FileWriter(note.path + Launcher.pathSeparator + note.uuid + Launcher.pathSeparator + "content.json")) {
+			file.write(obj.toJSONString());
+			file.flush();
+			System.out.println("Successfully Copied JSON Object to File...");
+			System.out.println("\nJSON Object: " + obj);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void writeMetaJSON(Note note) throws IOException {
+		JSONObject obj = new JSONObject();
+		obj.put("title", note.title);
+		obj.put("uuid", note.uuid);
+		obj.put("created_at", note.created_at);
+		obj.put("updated_at", note.updated_at);
+
+		JSONArray tags = new JSONArray();
+
+		if (note.tags.length > 0) for (String t : note.tags) tags.add(t);
+
+		obj.put("tags", tags);
+
+		if (!new File(note.path + Launcher.pathSeparator + note.uuid).exists()) {
+			if (!new File(note.path + Launcher.pathSeparator + note.uuid).mkdirs()) 
+				throw new IOException("Failed to create path " + note.path + Launcher.pathSeparator + note.uuid);
+		}
+		
+		try (FileWriter file = new FileWriter(note.path + Launcher.pathSeparator + note.uuid + Launcher.pathSeparator + "meta.json")) {
+			file.write(obj.toJSONString());
+			file.flush();
+			System.out.println("Successfully Copied JSON Object to File...");
+			System.out.println("\nJSON Object: " + obj);
+		}
+	}
+
+}
