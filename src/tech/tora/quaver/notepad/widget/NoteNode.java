5f7618f0022bd5d720bac237ff464f9768393b14
Nythril <Nythril@Bastion.Home>
2017-05-04 17:29:44 +0100

    Added functionality for searching list

--- src/tech/tora/quaver/notepad/widget/NoteNode.java
+++ src/tech/tora/quaver/notepad/widget/NoteNode.java
@@ -2,18 +2,18 @@ package tech.tora.quaver.notepad.widget;
 
 import tech.tora.quaver.list.SelectionList;
 import tech.tora.quaver.list.SelectionListNode;
-import tech.tora.quaver.structures.NoteMeta;
+import tech.tora.quaver.types.Note;
 
-public class NoteNode extends SelectionListNode {
+public abstract class NoteNode extends SelectionListNode {
 
-	public NoteMeta note;
+	public Note note;
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public NoteNode(SelectionList parent, String title, String subtitle, NoteMeta note) {
+	public NoteNode(SelectionList parent, String title, String subtitle, Note note) {
 		super(parent, title, subtitle);
 		this.note = note;
 	}
--- src/tech/tora/quaver/types/Note.java
+++ src/tech/tora/quaver/types/Note.java
@@ -1,11 +1,15 @@
 package tech.tora.quaver.types;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import tech.tora.quaver.Launcher;
 
@@ -21,7 +25,7 @@ public class Note {
 	public Note() {
 		// Do nothing
 	}
-	
+
 	public Note(String path, String title, String uuid, long created, long updated, String...tags) {
 		this.path = path;
 		this.title = title;
@@ -30,7 +34,7 @@ public class Note {
 		this.updated_at = updated;
 		this.tags = tags;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static void writeContentJSON(Note note) throws IOException {
 		JSONObject obj = new JSONObject();
@@ -52,7 +56,7 @@ public class Note {
 			if (!new File(note.path + Launcher.pathSeparator + note.uuid + ".qvnote").mkdirs()) 
 				throw new IOException("Failed to create path " + note.path + Launcher.pathSeparator + note.uuid + ".qvnote");
 		}
-		
+
 		try (FileWriter file = new FileWriter(note.path + Launcher.pathSeparator + note.uuid + ".qvnote" + Launcher.pathSeparator + "content.json")) {
 			file.write(obj.toJSONString());
 			file.flush();
@@ -79,7 +83,7 @@ public class Note {
 			if (!new File(note.path + Launcher.pathSeparator + note.uuid + ".qvnote").mkdirs()) 
 				throw new IOException("Failed to create path " + note.path + Launcher.pathSeparator + note.uuid + ".qvnote");
 		}
-		
+
 		try (FileWriter file = new FileWriter(note.path + Launcher.pathSeparator + note.uuid + ".qvnote" + Launcher.pathSeparator + "meta.json")) {
 			file.write(obj.toJSONString());
 			file.flush();
@@ -88,11 +92,86 @@ public class Note {
 		}
 	}
 
+	/**
+	 * Checks for the contents file and reads the values
+	 * @throws ParseException
+	 * @throws IOException
+	 */
+	@SuppressWarnings("unchecked")
+	public static Note readContentsJSON(String path) throws ParseException, IOException {
+		JSONParser parser = new JSONParser();
+
+		Object obj = parser.parse(new FileReader(path + Launcher.pathSeparator + "content.json"));
+		JSONObject jsonNoteObject = (JSONObject) obj;
+
+		String title = (String) jsonNoteObject.get("title");
+		JSONArray cellsListArray = (JSONArray) jsonNoteObject.get("cells");
+
+		Note newNote = new Note();
+		newNote.path = path;
+		newNote.title = title;
+
+		Iterator<JSONObject> cellsIterator = cellsListArray.iterator();
+		JSONObject nestedCellObject;
+		Cell tempCellForLoop;
+		while (cellsIterator.hasNext()) {
+			nestedCellObject = cellsIterator.next();
+			tempCellForLoop = new Cell();
+
+			tempCellForLoop.type = (String) nestedCellObject.get("type");
+			tempCellForLoop.language = (String) nestedCellObject.get("language");
+			tempCellForLoop.data = (String) nestedCellObject.get("data");
+
+			newNote.addCell(tempCellForLoop);
+		}
+		return newNote;
+	}
+
+	/**
+	 * Checks for the meta file and reads the values
+	 * @throws ParseException
+	 * @throws IOException
+	 */
+	public static Note readMetaJSON(String path) throws ParseException, IOException {
+		JSONParser parser = new JSONParser();
+
+		Object obj = parser.parse(new FileReader(path + Launcher.pathSeparator + "meta.json"));
+		JSONObject jsonNoteObject = (JSONObject) obj;
+
+		String title = (String) jsonNoteObject.get("title");
+		String id = (String) jsonNoteObject.get("uuid");
+		long created = (long) jsonNoteObject.get("created_at");
+		long updated = (long) jsonNoteObject.get("updated_at");
+		JSONArray tagsJSONArray = (JSONArray) jsonNoteObject.get("tags");
+
+		Note m = new Note();
+		m.path = path;
+		m.title = title;
+		m.uuid = id;
+		m.created_at = created;
+		m.updated_at = updated;
+
+		@SuppressWarnings("unchecked")
+		Iterator<String> cellsIterator = tagsJSONArray.iterator();
+		while (cellsIterator.hasNext()) {
+			String tag = cellsIterator.next();
+			m.addTag(tag);
+		}
+		return m;
+	}
+
+	public void addTag(String tag) {
+		String[] tmp = new String[tags.length+1];
+		for (int i = 0; i < tags.length; i++) tmp[i] = tags[i];
+		tmp[tmp.length-1] = tag;
+		tags = tmp;
+	}
+
 	public void addCell(Cell cell) {
 		Cell[] tmpArray = new Cell[cells.length+1];
 		for (int i = 0; i < cells.length; i++) tmpArray[i] = cells[i];
 		tmpArray[cells.length] = cell;
 		cells = tmpArray;
 	}
-	
+
 }
