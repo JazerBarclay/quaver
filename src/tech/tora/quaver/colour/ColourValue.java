45ac7ff0087be6c9b6660fc8d8a4500f427a785c
Nythril <Nythril@Bastion.Home>
2017-05-29 08:55:36 +0100

    Added handling for convertion

--- src/tech/tora/quaver/colour/ColourValue.java
+++ src/tech/tora/quaver/colour/ColourValue.java
@@ -9,35 +9,41 @@ public class ColourValue {
 	int r = 0;
 	int g = 0;
 	int b = 0;
-	
+
 	/**
 	 * This value is always calculated
 	 */
 	String hex;
-	
+
 	public ColourValue() {
 		// Do Nothing
 	}
-	
+
 	public ColourValue(int r, int g, int b) {
 		this.r = r;
 		this.g = g;
 		this.b = b;
 	}
-	
+
+	public ColourValue(String hex) {
+		if (hex != null) {
+			if (hex.length()==6) {
+				r = Integer.parseInt(hex.substring(0, 2), 16);
+				b = Integer.parseInt(hex.substring(2, 4), 16);
+				g = Integer.parseInt(hex.substring(4, 6), 16);
+			}	
+		}
+	}
 	public String getHex() {
-		hex = "";
-		return hex;
+		return String.format("%02x%02x%02x", r, g, b);
 	}
-	
+
 	public static String getHex(int r, int g ,int b) {
-		String localHex = "";
-		return localHex;
+		return String.format("%02x%02x%02x", r, g, b);
 	}
-	
+
 	public static String getHex(ColourValue v) {
-		String localHex = "";
-		return localHex;
+		return String.format("%02x%02x%02x", v.r, v.g, v.b);
 	}
-	
+
 }
