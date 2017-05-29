f8d07c3978cc594386232e9c6d4be11a857f0feb
Nythril <Nythril@Bastion.Home>
2017-05-29 08:55:59 +0100

    Upgraded theme to send hex string values and read into colour

--- src/tech/tora/quaver/Theme.java
+++ src/tech/tora/quaver/Theme.java
@@ -12,6 +12,8 @@ import tech.tora.quaver.colour.ColourValue;
 
 public class Theme {
 
+	private static String location = "res" + Launcher.pathSeparator + "themes";
+	
 	public String themeName = null;
 	public ColourValue fontColour = null;
 	public ColourValue wrapperFillColour = null;
@@ -35,20 +37,20 @@ public class Theme {
 	public static void writeThemeJSON(Theme t) throws IOException {
 		JSONObject obj = new JSONObject();
 		obj.put("theme_name", t.themeName);
-		obj.put("font_colour", t.fontColour);
-		obj.put("wrapper_fill_colour", t.wrapperFillColour);
-		obj.put("notebook_fill_colour", t.notebookFillColour);
-		obj.put("notebook_hover_colour", t.notebookHoverColour);
-		obj.put("note_fill_colour", t.noteFillColour);
-		obj.put("note_hover_colour", t.noteHoverColour);
-		obj.put("edit_fill_colour", t.editFillColour);
-		obj.put("preview_font_colour", t.previewFontColour);
-		obj.put("preview_fill_colour", t.previewFillColour);
-		obj.put("border_colour", t.borderColour);
+		obj.put("font_colour", t.fontColour.getHex());
+		obj.put("wrapper_fill_colour", t.wrapperFillColour.getHex());
+		obj.put("notebook_fill_colour", t.notebookFillColour.getHex());
+		obj.put("notebook_hover_colour", t.notebookHoverColour.getHex());
+		obj.put("note_fill_colour", t.noteFillColour.getHex());
+		obj.put("note_hover_colour", t.noteHoverColour.getHex());
+		obj.put("edit_fill_colour", t.editFillColour.getHex());
+		obj.put("preview_font_colour", t.previewFontColour.getHex());
+		obj.put("preview_fill_colour", t.previewFillColour.getHex());
+		obj.put("border_colour", t.borderColour.getHex());
 
-		if (!new File("res").exists()) if (!new File("res").mkdirs()) throw new IOException("Res directory could not be created");
+		if (!new File(location).exists()) if (!new File(location).mkdirs()) throw new IOException("Res directory could not be created");
 		
-		try (FileWriter file = new FileWriter("res" + Launcher.pathSeparator + t.themeName + ".json")) {
+		try (FileWriter file = new FileWriter(location + Launcher.pathSeparator + t.themeName + ".json")) {
 			file.write(obj.toJSONString());
 			file.flush();
 			System.out.println("\nSuccessfully Copied JSON Object to File...");
@@ -73,16 +75,16 @@ public class Theme {
 		JSONObject jsonObject = (JSONObject) obj;
 
 		t.themeName = (String) jsonObject.get("theme_name");
-		t.fontColour = (ColourValue) jsonObject.get("font_colour");
-		t.wrapperFillColour = (ColourValue) jsonObject.get("wrapper_fill_colour");
-		t.notebookFillColour = (ColourValue) jsonObject.get("notebook_fill_colour");
-		t.notebookHoverColour = (ColourValue) jsonObject.get("notebook_hover_colour");
-		t.noteFillColour = (ColourValue) jsonObject.get("note_fill_colour");
-		t.noteHoverColour = (ColourValue) jsonObject.get("note_hover_colour");
-		t.editFillColour = (ColourValue) jsonObject.get("edit_fill_colour");
-		t.previewFontColour = (ColourValue) jsonObject.get("preview_font_colour");
-		t.previewFillColour = (ColourValue) jsonObject.get("preview_fill_colour");
-		t.borderColour = (ColourValue) jsonObject.get("border_colour");
+		t.fontColour = new ColourValue((String) jsonObject.get("font_colour"));
+		t.wrapperFillColour = new ColourValue((String)  jsonObject.get("wrapper_fill_colour"));
+		t.notebookFillColour = new ColourValue((String) jsonObject.get("notebook_fill_colour"));
+		t.notebookHoverColour = new ColourValue((String) jsonObject.get("notebook_hover_colour"));
+		t.noteFillColour = new ColourValue((String) jsonObject.get("note_fill_colour"));
+		t.noteHoverColour = new ColourValue((String) jsonObject.get("note_hover_colour"));
+		t.editFillColour = new ColourValue((String) jsonObject.get("edit_fill_colour"));
+		t.previewFontColour = new ColourValue((String) jsonObject.get("preview_font_colour"));
+		t.previewFillColour = new ColourValue((String) jsonObject.get("preview_fill_colour"));
+		t.borderColour = new ColourValue((String) jsonObject.get("border_colour"));
 		
 		return t;
 	}
