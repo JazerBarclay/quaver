94d0098fe1f06760077b1de20524232a737003b6
Nythril <Nythril@Bastion.Home>
2017-05-30 12:40:49 +0100

    Converter class created for churning out markdown in html

---
+++ src/tech/tora/quaver/markdown/Converter.java
@@ -0,0 +1,14 @@
+package tech.tora.quaver.markdown;
+
+public class Converter {
+
+	/**
+	 * Takes a markdown string and converts it to html code for displaying
+	 * @param markdownString
+	 * @return HTML in div for addition to body
+	 */
+	public static String convertToHTML(String markdownString) {
+		return "";
+	}
+	
+}
