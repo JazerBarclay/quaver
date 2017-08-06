f9d58ae2625f220115020cc375ea8eadb9deaa9a
nythril <nythril@protonmail.com>
2017-08-06 12:01:20 +0100

    Comments

--- src/tech/tora/tools/swing/panel/PaneHorizontal.java
+++ src/tech/tora/tools/swing/panel/PaneHorizontal.java
@@ -7,58 +7,93 @@ import javax.swing.JPanel;
 
 public class PaneHorizontal extends JPanel {
 
-	/**
-	 * 
-	 */
+	/** * **/
 	private static final long serialVersionUID = 1L;
-
-	private JPanel leftPane, centerPane, rightPane;
 	
-	public PaneHorizontal(Color c) {
+	/** Left border panel for left section content **/
+	private JPanel leftPane;
+	
+	/** Center border panel for center section content **/
+	private JPanel centerPane;
+	
+	/** Right border panel for right section content **/
+	private JPanel rightPane;
+	
+	
+	/* ------------------------------------------------------ */
+	// Constructors and Initialisation
+	/* ------------------------------------------------------ */
+	
+	/**
+	 * Initialises the horizontal pane with a base colour
+	 * @param colour
+	 */
+	public PaneHorizontal(Color colour) {
 		this.setLayout(new BorderLayout());
 		leftPane = new JPanel(new BorderLayout());
 		centerPane = new JPanel(new BorderLayout());
 		rightPane = new JPanel(new BorderLayout());
 
-		if (c != null) {
-			leftPane.setBackground(c);
-			centerPane.setBackground(c);
-			rightPane.setBackground(c);
+		if (colour != null) {
+			leftPane.setBackground(colour);
+			centerPane.setBackground(colour);
+			rightPane.setBackground(colour);
 		}
 		
 		add(leftPane, BorderLayout.WEST);
 		add(centerPane, BorderLayout.CENTER);
 		add(rightPane, BorderLayout.EAST);
 	}
+	
 
+	/* ------------------------------------------------------ */
+	// Core Methods
+	/* ------------------------------------------------------ */
+	
+	/**
+	 * Sets the colour of all content panes and the wrapper pane
+	 * @param colour
+	 */
+	public void setColour(Color colour) {
+		leftPane.setBackground(colour);
+		centerPane.setBackground(colour);
+		rightPane.setBackground(colour);
+	}
+	
+	
+	/* ------------------------------------------------------ */
+	// Getters and Setters
+	/* ------------------------------------------------------ */
+	
+	/** Returns the left panel **/
 	public JPanel getLeftPane() {
 		return leftPane;
 	}
-
+	
+	/** Sets the left panel **/
 	public void setLeftPane(JPanel leftPane) {
 		this.leftPane = leftPane;
 	}
 
+	/** Returns the center panel **/
 	public JPanel getCenterPane() {
 		return centerPane;
 	}
 
+	/** Sets the center panel **/
 	public void setCenterPane(JPanel centerPane) {
 		this.centerPane = centerPane;
 	}
 
+	/** Returns the right panel **/
 	public JPanel getRightPane() {
 		return rightPane;
 	}
 
+	/** Sets the right panel **/
 	public void setRightPane(JPanel rightPane) {
 		this.rightPane = rightPane;
 	}
 	
-	public void setColour(Color c) {
-		leftPane.setBackground(c);
-		centerPane.setBackground(c);
-		rightPane.setBackground(c);
-	}
 	
 }
--- src/tech/tora/tools/swing/panel/PaneVertical.java
+++ src/tech/tora/tools/swing/panel/PaneVertical.java
@@ -7,58 +7,93 @@ import javax.swing.JPanel;
 
 public class PaneVertical extends JPanel {
 
-	/**
-	 * 
-	 */
+	/** * **/
 	private static final long serialVersionUID = 1L;
 
-	private JPanel headerPane, centerPane, footerPane;
+	/** Header border panel for header section content **/
+	private JPanel headerPane;
 	
-	public PaneVertical(Color c) {
+	/** Center border panel for header section content **/
+	private JPanel centerPane;
+	
+	/** Footer border panel for header section content **/
+	private JPanel footerPane;
+	
+	
+	/* ------------------------------------------------------ */
+	// Constructors and Initialisation
+	/* ------------------------------------------------------ */
+	
+	/**
+	 * Initialise the vertical pane with a base colour
+	 * @param colour
+	 */
+	public PaneVertical(Color colour) {
 		this.setLayout(new BorderLayout());
 		headerPane = new JPanel(new BorderLayout());
 		centerPane = new JPanel(new BorderLayout());
 		footerPane = new JPanel(new BorderLayout());
 
-		if (c != null) {
-			headerPane.setBackground(c);
-			centerPane.setBackground(c);
-			footerPane.setBackground(c);
+		if (colour != null) {
+			headerPane.setBackground(colour);
+			centerPane.setBackground(colour);
+			footerPane.setBackground(colour);
 		}
 		
 		add(headerPane, BorderLayout.NORTH);
 		add(centerPane, BorderLayout.CENTER);
 		add(footerPane, BorderLayout.SOUTH);
 	}
-
+	
+	
+	/* ------------------------------------------------------ */
+	// Core Methods
+	/* ------------------------------------------------------ */
+	
+	/**
+	 * Sets the colour of all content panes and the wrapper pane
+	 * @param colour
+	 */
+	public void setColour(Color colour) {
+		headerPane.setBackground(colour);
+		centerPane.setBackground(colour);
+		footerPane.setBackground(colour);
+	}
+	
+	
+	/* ------------------------------------------------------ */
+	// Getters and Setters
+	/* ------------------------------------------------------ */
+	
+	/** Returns the header panel **/
 	public JPanel getHeaderPane() {
 		return headerPane;
 	}
 
+	/** Sets the header panel **/
 	public void setHeaderPane(JPanel headerPane) {
 		this.headerPane = headerPane;
 	}
 
+	/** Returns the center panel **/
 	public JPanel getCenterPane() {
 		return centerPane;
 	}
 
+	/** Sets the center panel **/
 	public void setCenterPane(JPanel centerPane) {
 		this.centerPane = centerPane;
 	}
 
+	/** Returns the footer panel **/
 	public JPanel getFooterPane() {
 		return footerPane;
 	}
 
+	/** Sets the footer panel **/
 	public void setFooterPane(JPanel footerPane) {
 		this.footerPane = footerPane;
 	}
-	
-	public void setColour(Color c) {
-		headerPane.setBackground(c);
-		centerPane.setBackground(c);
-		footerPane.setBackground(c);
-	}
+
 	
 }
