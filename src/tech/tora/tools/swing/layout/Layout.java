329e1e87b2f36259124c15d6540dfb6aacd0d5bd
nythril <nythril@protonmail.com>
2017-08-16 16:49:58 +0100

    Reworked because of bad practice
    Methods were modifying parameters rather than returning the result

--- src/tech/tora/tools/swing/layout/Layout.java
+++ src/tech/tora/tools/swing/layout/Layout.java
@@ -2,69 +2,85 @@ package tech.tora.tools.swing.layout;
 
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-import tech.tora.quaver.theme.Theme;
-import tech.tora.tools.swing.frame.AdvancedFrame;
 
+/**
+ * Standard template for standard JFrame building
+ * @author Nythril
+ */
 public abstract class Layout {
 
-	private String title;
-	private static int width = -1, height = -1;
 	private static int defaultWidth = 800, defaultHeight = 600;
+	// Positioning to be managed later
 
-	protected AdvancedFrame parent;
-	protected JMenuBar topMenu;
-	protected JPanel wrapper;
-
-	public Layout(AdvancedFrame parent, Theme theme) {
-		this.parent = parent;
-		initVariables();
-		buildFrame(theme);
-		constructFrame(wrapper);
-		buildElements();
-		constructElements();
-		constructTopBar(topMenu);
+	public Layout() {
+		
 	}
 
-	/** Initialise variables **/
-	private final void initVariables() {
-		topMenu = new JMenuBar();
-		wrapper = new JPanel();
-	}
-
+	/**
+	 * Returns the name of the program used in the menu and as part of the title
+	 * @return String - name
+	 */
+	public abstract String getName();
 
 	/* ------------------------------------------------------ */
 	// Frame Content Management
 	/* ------------------------------------------------------ */
 	
-	public abstract void buildFrame(Theme theme);
-	public abstract void constructFrame(JPanel wrapperPanel);
-	public abstract void constructTopBar(JMenuBar menu);
-	public abstract void buildElements();
-	public abstract void constructElements();
-
+	/**
+	 * Returns the menu used in the frame
+	 * @return JMenuBar - menu
+	 */
+	public abstract JMenuBar getMenu();
+	
+	/**
+	 * Returns the most top level wrapper of the frame layout
+	 * @return JPanel - wrapper
+	 */
+	public abstract JPanel getWrapper();
+	
+	/**
+	 * Returns the title used in the frame heading
+	 * @return String - title
+	 */
+	public abstract String getTitle();
+	
 	/* ------------------------------------------------------ */
 	// Window Action Overrides
 	/* ------------------------------------------------------ */
 
+	/**
+	 * Window Opening Action
+	 */
 	public abstract void windowOpenAction();
+	
+	/**
+	 * Window Closing Action
+	 */
 	public abstract void windowCloseAction();
+	
+	/**
+	 * Window Minimise Action
+	 */
 	public abstract void windowMinimiseAction();
+	
+	/**
+	 * Window Maximise Action
+	 */
 	public abstract void windowMaximiseAction();
+	
+	/**
+	 * Window Focused Action
+	 */
 	public abstract void windowGainFocusAction();
+	
+	/**
+	 * Window LoS Action
+	 */
 	public abstract void windowLoseFocusAction();
+	
 
 	/* ------------------------------------------------------ */
-
-	/** Returns the menu bar **/
-	public final JMenuBar getMenu() {
-		return topMenu;
-	}
-
-	/** Returns the content wrapper pane **/
-	public final JPanel getWrapper() {
-		return wrapper;
-	}
-
+	// Frame Defaults
 	/* ------------------------------------------------------ */
 
 	/** Default width set by the layout standard **/
@@ -86,37 +102,5 @@ public abstract class Layout {
 	public int getDefaultHeight() {
 		return defaultHeight;
 	}
-
-	/** Sets the active width of the layout **/
-	public void setWidth(int w) {
-		width = w;
-		parent.updateFrameSize(w, parent.getHeight(), true);
-	}
-
-	/** Sets the active height of the layout **/
-	public void setHeight(int h) {
-		height = h;
-		parent.updateFrameSize(parent.getWidth(), h, true);
-	}
-
-	/** Returns the active width of the layout **/
-	public int getWidth() {
-		return width;
-	};
-
-	/** Returns the active height of the layout **/
-	public int getHeight() {
-		return height;
-	};
-
-	/** Sets the layout title **/
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	/** Returns the layout title **/
-	public String getTitle() {
-		return title;
-	};
 
 }
