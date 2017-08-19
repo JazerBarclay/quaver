7f5afee218ecff411b4a0084042d883c3cdca25a
nythril <nythril@protonmail.com>
2017-08-19 14:22:23 +0100

    Comments added

--- src/tech/tora/quaver/notepad/layout/CustomLayout.java
+++ src/tech/tora/quaver/notepad/layout/CustomLayout.java
@@ -7,7 +7,7 @@ import tech.tora.quaver.theme.Theme;
 import tech.tora.tools.swing.layout.Layout;
 
 /**
- * Currently just an unused abstraction layer for quick maintenance on the core Layout class
+ * Basic management for future layouts to keep things neato
  * @author Nythril
  */
 public abstract class CustomLayout extends Layout {
@@ -24,10 +24,39 @@ public abstract class CustomLayout extends Layout {
 		constructElements();
 	}
 	
+	/* ------------------------------------------------------ */
+	// Layout Generation
+	/* ------------------------------------------------------ */
+	
+	/**
+	 * Create panels
+	 * @param theme
+	 * @return
+	 */
 	public abstract JPanel buildFrame(Theme theme);
+	
+	/**
+	 * Combine panels to generate layout template 
+	 * @param wrapper
+	 * @return
+	 */
 	public abstract JPanel constructFrame(JPanel wrapper);
+	
+	/**
+	 * Create elements for layout
+	 * @param theme
+	 */
 	public abstract void buildElements(Theme theme);
+	
+	/**
+	 * Add elements to the layout template
+	 */
 	public abstract void constructElements();
+	
+	
+	/* ------------------------------------------------------ */
+	// Standard Layout Overrides
+	/* ------------------------------------------------------ */
 	
 	@Override
 	public String getName() {
