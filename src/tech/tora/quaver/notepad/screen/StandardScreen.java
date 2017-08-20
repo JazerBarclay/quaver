afe66b896f1fffdfe6a4cd8951b91878fc8a7b3c
nythril <nythril@protonmail.com>
2017-08-20 16:15:55 +0100

    Some more overrides managed

--- src/tech/tora/quaver/notepad/screen/StandardScreen.java
+++ src/tech/tora/quaver/notepad/screen/StandardScreen.java
@@ -1,5 +1,7 @@
 package tech.tora.quaver.notepad.screen;
 
+import java.awt.Font;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
@@ -10,26 +12,64 @@ import tech.tora.quaver.types.Cell;
 import tech.tora.quaver.types.Library;
 import tech.tora.quaver.types.Note;
 import tech.tora.quaver.types.Notebook;
+import tech.tora.tools.swing.list.BasicClickListNode;
+import tech.tora.tools.swing.list.BasicListNode;
+import tech.tora.tools.swing.list.ClickListener;
 
 public class StandardScreen extends StandardLayout {
 
+	private Library activeLibrary = null;
+	private Notebook activeNotebook = null;
+	private Note activeNote = null;
+	
 	public StandardScreen(Configuration config, Theme theme) {
 		super(theme);
 	}
 
 	@Override
 	public void addLibrary(Library library) {
-		
+		notebooksList.addNode(new BasicListNode(20, library.getPath() + library.getName(), library.getName(), 
+				theme.notebookFillColour.addShade(-15, -15, -15), theme.notebookHoverColour.getAsColor(), 
+				new Font("Helvetica", Font.BOLD, 10), theme.fontColour.getAsColor(), 0) {
+		});
 	}
 
 	@Override
 	public void addNotebook(Notebook notebook) {
-		
+		ClickListener clickEvent = new ClickListener() {
+			@Override
+			public void onClick() {
+				System.out.println("Test");
+			}
+		};
+		notebooksList.addNode(new BasicClickListNode(25, notebook.getUUID(), "   " + notebook.getName(), 
+				theme.notebookFillColour.getAsColor(), theme.notebookHoverColour.getAsColor(), 
+				new Font("Helvetica", Font.BOLD, 12), theme.fontColour.getAsColor(), -10) {
+			@Override
+			public void onClick() {
+				notebooksList.onClick(this);
+				clickEvent.onClick();
+			}
+		});
 	}
 
 	@Override
 	public void addNote(Note note) {
-		
+		ClickListener clickEvent = new ClickListener() {
+			@Override
+			public void onClick() {
+				System.out.println("Test2");
+			}
+		};
+		notesList.addNode(new BasicClickListNode(40, note.getUUID(), note.getTitle(), 
+				theme.notebookFillColour.getAsColor(), theme.notebookHoverColour.getAsColor(), 
+				new Font("Helvetica", Font.BOLD, 12), theme.fontColour.getAsColor(), -10) {
+			@Override
+			public void onClick() {
+				notesList.onClick(this);
+				clickEvent.onClick();
+			}
+		});
 	}
 
 	@Override
@@ -123,13 +163,14 @@ public class StandardScreen extends StandardLayout {
 	}
 
 	@Override
+	@Deprecated
 	public void updatePreview(Cell[] cells) {
-		
+		// Do Nothing
 	}
 
 	@Override
 	public void updatePreview(String notes) {
-		
+		previewArea.setText(notes);
 	}
 
 	@Override
@@ -142,17 +183,17 @@ public class StandardScreen extends StandardLayout {
 
 	@Override
 	public void setActiveLibrary(Library library) {
-		
+		this.activeLibrary = library;
 	}
 
 	@Override
 	public void setActiveNotebook(Notebook notebook) {
-		
+		this.activeNotebook = notebook;
 	}
 
 	@Override
 	public void setActiveNote(Note note) {
-
+		this.activeNote = note;
 	}
 	
 }
