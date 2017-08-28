5f270e5443de1da807c179f7e17884ac0af44514
nythril <nythril@protonmail.com>
2017-08-28 17:05:25 +0100

    Comments removed due to them taking up room and being useless

--- src/tech/tora/quaver/Configuration.java
+++ src/tech/tora/quaver/Configuration.java
@@ -13,62 +13,22 @@ import org.json.simple.parser.ParseException;
 
 public class Configuration {
 	
-	/** Name of the configuration **/
 	private String name;
-	
-	/** Theme name used for the interface on load **/
 	private String theme;
-	
-	/** For programmers and debugging for clients during beta **/
 	private boolean devmode;
-	
-	/** Array of all library locations across the file system **/
 	private String[] libraries;
-	
-	
-	/* ------------------------------------------------------ */
-	// Constructors and Initialisation
-	/* ------------------------------------------------------ */
-	
-	/**
-	 * Initialise the configuration without specifying any fields
-	 */
+
 	public Configuration() {
 		this(null, null, false);
 	}
 	
-	/**
-	 * Initialise the configuration with all required fields
-	 * 
-	 * @param name
-	 * @param theme
-	 * @param devmode
-	 */
 	public Configuration(String name, String theme, boolean devmode) {
 		this.name = name;
 		this.theme = theme;
 		this.devmode = devmode;
-		init();
-	}
-
-	/**
-	 * Initialise variables and arrays
-	 */
-	private void init() {
 		libraries = new String[] {};
 	}
 	
-	
-	/* ------------------------------------------------------ */
-	// Core Methods
-	/* ------------------------------------------------------ */
-	
-	/**
-	 * Writes a blank configuration file
-	 * 
-	 * @param config - Template configuration to be saved
-	 * @throws IOException
-	 */
 	@SuppressWarnings("unchecked")
 	public static void writeConfigJSON(Configuration config) throws IOException {
 		JSONObject obj = new JSONObject();
@@ -76,12 +36,13 @@ public class Configuration {
 		obj.put("devmode", config.devmode);
 		obj.put("theme", config.theme);
 
-		JSONArray libraries = new JSONArray();
+		JSONArray librariesJSON = new JSONArray();
 		
-		for (String notebook : config.libraries) libraries.add(notebook);
-		obj.put("libraries", libraries);
+		for (String notebook : config.libraries) librariesJSON.add(notebook);
+		obj.put("libraries", librariesJSON);
 
-		if (!new File("res").exists()) if (!new File("res").mkdirs()) throw new IOException("Res directory could not be created");
+		if (!new File("res").exists()) if (!new File("res").mkdirs())
+					throw new IOException("Res directory could not be created");
 		
 		try (FileWriter file = new FileWriter("res" + File.separator + "config.json")) {
 			file.write(obj.toJSONString());
@@ -91,14 +52,6 @@ public class Configuration {
 		}
 	}
 	
-	/**
-	 * Reads the configuration file
-	 * 
-	 * @return - Configuration file settings
-	 * 
-	 * @throws ParseException - Structure or variable failure
-	 * @throws IOException - Read file failed
-	 */
 	@SuppressWarnings("unchecked")
 	public static Configuration readConfigJSON() throws IOException, ParseException {
 		JSONParser parser = new JSONParser();
@@ -132,55 +85,32 @@ public class Configuration {
 	// Getters and Setters
 	/* ------------------------------------------------------ */
 	
-	/**
-	 * @param name the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the name
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * Sets the theme
-	 * @param theme the theme to set
-	 */
 	public void setTheme(String theme) {
 		this.theme = theme;
 	}
-
-	/**
-	 * @return the theme
-	 */
+	
 	public String getTheme() {
 		return theme;
 	}
 
-	/**
-	 * @param devmode
-	 */
 	public void setDevmode(boolean devmode) {
 		this.devmode = devmode;
 	}
-
-	/**
-	 * @return devmode
-	 */
+	
 	public boolean isDevmode() {
 		return devmode;
 	}
 
-	/**
-	 * @return libraries
-	 */
 	public String[] getLibraries() {
 		return libraries;
 	}
-
 	
 }
