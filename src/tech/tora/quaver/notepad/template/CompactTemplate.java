29f6cd60c90b58178ddf7441af0fe0f14d575870
nythril <nythril@protonmail.com>
2017-09-01 23:31:25 +0100

    Start of compact template

---
+++ src/tech/tora/quaver/notepad/template/CompactTemplate.java
@@ -0,0 +1,93 @@
+package tech.tora.quaver.notepad.template;
+
+import javax.swing.JPanel;
+
+import tech.tora.quaver.theme.Theme;
+
+public class CompactTemplate extends QuaverTemplate {
+
+	public CompactTemplate(Theme theme) {
+		super(theme);
+	}
+
+	@Override
+	public JPanel getLibraryListPanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getNotebookListPanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getNoteListPanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getLibraryTitlePanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getNotebookTitlePanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getNoteTitlePanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getEditTitlePanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getEditAreaPanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getEditTagPanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getPreviewAreaPanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getPreviewStatusPanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getAddLibraryButtonPanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getAddNotebookButtonPanel() {
+		return null;
+	}
+
+	@Override
+	public JPanel getAddNoteButtonPanel() {
+		return null;
+	}
+
+	@Override
+	protected JPanel buildPanels(Theme theme) {
+		return null;
+	}
+
+	@Override
+	protected void constructPanels(JPanel wrapper) {
+		
+	}
+
+}
